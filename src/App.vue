7560889b3798132ef8df65f51565389dfa8bcdfb
Alexandra <pinheiro.alexandra21@gmail.com>
2022-10-04 15:25:12 +0200

    MainLayout smartphone/tablette finish

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,11 @@ export default {
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+ 
+  body {
+    
     background-color: black;
     color:white;
-  }
-  body {
-    background-color: black;
   }
 
 </style>
--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -5,10 +5,7 @@
                 <h2>Alexandra Pinheiro</h2>
                 <p>Développeuse Web Vue.js</p>
                 <div class="profile-image">
-                    <img src="../assets/personal-img.png" alt="profile-image">
-                </div>
-                <div class="square">
-
+                    <img src="../assets/personal-img.png" class="perso-img" alt="profile-image">
                 </div>
             </div>
             <!--<div class="social-links">
@@ -36,51 +33,17 @@
 </script>
 
 <style>
-    .square {
-        background-color: rgb(255, 103, 99);
+    .block-container {
+        margin: 10px;
     }
-
-@media screen and (max-width: 768px) {
-    .container-one {
-        height: 100%;
-        display: block;
-        justify-content: center;
-    }
-    .profile {
-        align-items: flex-start;
-        display: flex;
-        flex-direction: column;
-        padding-left: 10px;
-        margin: 10px; 
+    h2, p {
+    font-family: Impact, Charcoal, Helvetica Inserat, Bitstream Vera Sans Bold, Arial Black, sans serif;
     }
     h2 {
-        font-size: xx-large;
-        font-weight: 900;
-        margin: 5px;
+        text-transform: uppercase;
     }
-    p {
-        margin: 5px;
-    }
-    .profile-image {
-        align-self: flex-end;
-    }
-    img {
-        width: 11em;
-        align-self: flex-end;
-        margin: 60px 70px;
-        position: absolute;
-        top: 30em;
-        right: 10px;
-    }
-    .square {
-        display: flex;
-        height: 250px;
-        width: 170px;
-        position: static;
-        margin: 60px 50px;
-    }
-}
-@media screen and (min-width: 768px) {
+    
+@media only screen and (max-width: 767px) {
     .profile {
         align-items: flex-start;
         display: flex;
@@ -89,31 +52,66 @@
         margin: 20px; 
     }
     h2 {
-        font-size: xxx-large;
+        font-size: 20px;
         margin: 10px;
     }
     p {
-        font-size: x-large;
         margin: 10px;
-
     }
     .profile-image {
-        align-self: flex-end;
+        align-self: center;
+        margin: 60px 40px;
     }
-    img {
-        top: 40em;
-        width: 15em;
-        margin: 60px 200px;
-    }   
-    .square {
-        height: 350px;
-        width: 270px;
-        margin: 150px 200px 20px;
+    .perso-img {
+        width: 11em;
+        box-shadow: -40px 50px rgb(255 103 99);
     }
 }
 
-    .img {
-        width: 30rem;
+@media only screen and (min-width: 768px) {
+    .profile {
+        align-items: flex-start;
+        display: flex;
+        flex-direction: column;
+        padding-left: 10px;
+        margin: 20px; 
     }
-    
+    h2 {
+        font-size: 40px;
+        margin: 20px;
+    }
+    p {
+        font-size: x-large;
+        margin: 20px;
+
+    }
+    .profile-image {
+        align-self: center;
+        margin: 60px 40px;
+    }
+    .perso-img {
+        width: 20em;
+        box-shadow: -40px 50px rgb(255 103 99);
+    }   
+}
+
+@media only screen and (min-width: 1200px) {
+    h2 {
+        font-size: 50px;
+        margin: 20px 40px;
+    }
+    p {
+        font-size: 30px;
+        margin: 20px 40px;
+
+    }
+    .profile-image {
+        align-self: center;
+        margin: 60px;
+    }
+    .perso-img {
+        width: 30em;
+        box-shadow: -60px 70px rgb(255 103 99);
+    }
+}
 </style>
